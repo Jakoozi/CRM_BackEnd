357f0061b9f0dbfcda7da50fa1646295ce8b2c82
darkwind007 <somatic20@gmail.com>
2020-05-29 18:18:46 +0100

    added pagination

---
+++ Xend.CRM/Xend.CRM.ServiceLayer/PaginationService/PaginatedResultService.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xend.CRM.Core.ServiceLayer;
+using Xend.CRM.ModelLayer.ResponseModel;
+
+namespace Xend.CRM.ServiceLayer.PaginationService
+{
+    public class PaginatedResultService : IPaginatedResultService
+    {
+        IMapper Mapper { get; }
+        public PaginatedResultService(IMapper mapper)
+        {
+            Mapper = mapper;
+        }
+        public async Task<PaginatedResult<T>> PaginateRecordsAsync<T, W>(int page, int pageSize, IEnumerable<W> records, Func<IEnumerable<W>, Task<IEnumerable<T>>> modelProcessorDelegate)
+            where T : class
+        {
+            int recordsToSkip = (page - 1) * pageSize;
+            int totalRecords = records.Count();
+            double totalPages = (double)totalRecords / (double)pageSize;
+
+            int pageCount = int.Parse(Math.Ceiling(totalPages).ToString());
+
+            IEnumerable<W> paginatedRecords = records.Skip(recordsToSkip).Take(pageSize);
+
+            IEnumerable<T> paginatedRecordsDTO = await modelProcessorDelegate(paginatedRecords.ToList());
+            PaginatedResult<T> paginatedResult = new PaginatedResult<T>
+            {
+                Records = paginatedRecordsDTO,
+                CurrentPage = page,
+                PageSize = pageSize,
+                TotalPages = pageCount,
+                TotalRecords = totalRecords
+            };
+            return paginatedResult;
+        }
+
+
+
+        public PaginatedResult<T> PaginateRecords<T, W>(int page, int pageSize, IEnumerable<W> records, Func<IEnumerable<W>, IEnumerable<T>> modelProcessorDelegate)
+            where T : class
+        {
+            int recordsToSkip = (page - 1) * pageSize;
+            int totalRecords = records.Count();
+            double totalPages = (double)totalRecords / (double)pageSize;
+
+            int pageCount = int.Parse(Math.Ceiling(totalPages).ToString());
+
+            IEnumerable<W> paginatedRecords = records.Skip(recordsToSkip).Take(pageSize);
+
+            IEnumerable<T> paginatedRecordsDTO = modelProcessorDelegate(paginatedRecords.ToList());
+            PaginatedResult<T> paginatedResult = new PaginatedResult<T>
+            {
+                Records = paginatedRecordsDTO,
+                CurrentPage = page,
+                PageSize = pageSize,
+                TotalPages = pageCount,
+                TotalRecords = totalRecords
+            };
+            return paginatedResult;
+        }
+
+
+
+        public PaginatedResult<T> PaginateRecords<T, W>(int page, int pageSize, IEnumerable<W> records)
+            where T : class
+            where W : class
+        {
+            int recordsToSkip = (page - 1) * pageSize;
+            int totalRecords = records.Count();
+            double totalPages = (double)totalRecords / (double)pageSize;
+
+            int pageCount = int.Parse(Math.Ceiling(totalPages).ToString());
+
+            IEnumerable<W> paginatedRecords = records.Skip(recordsToSkip).Take(pageSize);
+            IEnumerable<T> paginatedRecordsDTO = Mapper.Map<IEnumerable<T>>(paginatedRecords);
+
+            PaginatedResult<T> paginatedResult = new PaginatedResult<T>
+            {
+                Records = paginatedRecordsDTO,
+                CurrentPage = page,
+                PageSize = pageSize,
+                TotalPages = pageCount,
+                TotalRecords = totalRecords
+            };
+            return paginatedResult;
+        }
+    }
+}
